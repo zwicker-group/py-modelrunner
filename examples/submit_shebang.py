137977d49c8f261df11cdcda22233c780071e2d7
David Zwicker <david.zwicker@ds.mpg.de>
2022-02-10 16:45:38 +0100

    Improved description of example

--- examples/submit_shebang.py
+++ examples/submit_shebang.py
@@ -2,9 +2,10 @@
 """
 This example shows how to submit a model to a queuing system using the magic line above.
 
-Note that the method `local` just runs the script locally, thus not really queuing. To
-actually queue a job on a high performance computing cluster, replace the `method`
-argument by something more suitable; see the documentation. 
+Note that the method `local` just runs the script locally using a background process.
+The script will thus start the job and return to the prompt immediately, thus not really
+queuing. To actually queue a job on a high performance computing cluster, replace the
+`method` argument by something more suitable; see the documentation. 
 
 .. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de>
 """
