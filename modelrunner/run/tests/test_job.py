badcd7f522aeaeab1680aee6a55101d85acb9a2b
David Zwicker <david.zwicker@ds.mpg.de>
2022-11-15 10:00:42 +0100

    Allow keeping iterable parameters

--- modelrunner/run/tests/test_job.py
+++ modelrunner/run/tests/test_job.py
@@ -36,15 +36,16 @@ def test_submit_job(tmp_path):
 def test_submit_jobs(tmp_path):
     """test some edge cases of the submit_jobs function"""
 
-    def run(**kwargs):
+    def run(parameters, **kwargs):
         """helper submitting job locally"""
         submit_jobs(
             SCRIPT_PATH / "function.py",
             tmp_path,
-            parameters=kwargs.copy(),
+            parameters=parameters.copy(),
             log_folder=tmp_path,
             method="foreground",
             overwrite_strategy="silent_overwrite",
+            **kwargs,
         )
 
         # read result
@@ -56,12 +57,19 @@ def test_submit_jobs(tmp_path):
 
         return col
 
-    res = run(a=(1, 2))["a"]
+    res = run({"a": (1, 2)})["a"]
     # the order of the results might not be deterministic => sort result
     np.testing.assert_allclose(np.sort(res), [1, 2])
 
-    res = run(b=[[1, 2], [3, 4]])["b"]
+    res = run({"b": [[1, 2], [3, 4]]})["b"]
     # the order of the results might not be deterministic => test both variants
     test1 = np.allclose(res[0], [1, 2]) and np.allclose(res[1], [3, 4])
     test2 = np.allclose(res[0], [3, 4]) and np.allclose(res[1], [1, 2])
     assert test1 or test2
+
+    res = run({"b": [1, 2]}, keep_list=["b"])
+    # the order of the results might not be deterministic => test both variants
+    assert len(res) == 1
+    print(res)
+    assert res["a"][0] == 1
+    np.testing.assert_allclose(res["b"][0], [1, 2])
