1e2673d53adc8cb50fed15e18f64a831e1be530f
David Zwicker <david.zwicker@ds.mpg.de>
2023-03-24 15:30:06 +0100

    Added more tests for attributes

--- tests/state/test_state.py
+++ tests/state/test_state.py
@@ -7,7 +7,13 @@ import copy
 import numpy as np
 import pytest
 
-from modelrunner.state import ArrayCollectionState, DictState, ObjectState, StateBase
+from modelrunner.state import (
+    ArrayCollectionState,
+    ArrayState,
+    DictState,
+    ObjectState,
+    StateBase,
+)
 from modelrunner.state.base import _equals
 from utils.states import EXTENSIONS, get_states
 
@@ -50,6 +56,80 @@ def test_empty_state_io(state_cls, ext, tmp_path):
     state.to_file(path)
     state2 = StateBase.from_file(path)
     assert state == state2
+
+
+@pytest.mark.parametrize(
+    "state_cls", [ArrayState, DictState, ObjectState, ArrayCollectionState]
+)
+def test_state_attributes_implicit(state_cls, tmp_path):
+    """test simple state IO with implicitely defined attributes"""
+
+    class MyState(state_cls):
+        _state_attributes_attr_name = "attrs"
+
+        def __init__(self, value):
+            self.attrs = {"value": value}
+            if state_cls is ArrayState:
+                self.data = np.arange(5)
+            elif state_cls is DictState:
+                self.data = {"a": ArrayState(np.arange(5))}
+            elif state_cls is ArrayCollectionState:
+                self.data = (np.arange(5),)
+            else:
+                self.data = None
+
+    state = MyState("hello")
+    state2 = state.copy()
+    assert state == state2
+    assert state2.attrs["value"] == "hello"
+
+    for ext in EXTENSIONS:
+        path = tmp_path / ("file." + ext)
+        state.to_file(path)
+        state2 = StateBase.from_file(path)
+        assert state == state2
+        assert state2.attrs["value"] == "hello"
+
+
+@pytest.mark.parametrize(
+    "state_cls", [ArrayState, DictState, ObjectState, ArrayCollectionState]
+)
+def test_state_attributes_explicit(state_cls, tmp_path):
+    """test simple state IO with explicitely defined attributes"""
+
+    class MyState(state_cls):
+        def __init__(self, value):
+            self.value = value
+            if state_cls is ArrayState:
+                self.data = np.arange(5)
+            elif state_cls is DictState:
+                self.data = {"a": ArrayState(np.arange(5))}
+            elif state_cls is ArrayCollectionState:
+                self.data = (np.arange(5),)
+            else:
+                self.data = None
+
+        @property
+        def _state_attributes(self):
+            attrs = super()._state_attributes
+            attrs["value"] = self.value
+            return attrs
+
+        @_state_attributes.setter
+        def _state_attributes(self, attrs):
+            self.value = attrs["value"]
+
+    state = MyState("hello")
+    state2 = state.copy()
+    assert state == state2
+    assert state2.value == "hello"
+
+    for ext in EXTENSIONS:
+        path = tmp_path / ("file." + ext)
+        state.to_file(path)
+        state2 = StateBase.from_file(path)
+        assert state == state2
+        assert state2.value == "hello"
 
 
 @pytest.mark.parametrize("ext", EXTENSIONS)
